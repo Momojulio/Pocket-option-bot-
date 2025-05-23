0ad4dc9970ac0fba5aee1ec850e6321619853a3c
Momojulio <momojulio777@gmail.com>
2025-05-23 16:30:43 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
+import os
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+USE_WEBHOOK = os.getenv("USE_WEBHOOK", "True").lower() == "true"
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+TRADE_AMOUNT = float(os.getenv("TRADE_AMOUNT", 1))
+USE_REAL_ACCOUNT = os.getenv("USE_REAL_ACCOUNT", "False").lower() == "true"
