ab942d94f80cc067982f21953f193cc1f0e7fe89
Momojulio <momojulio777@gmail.com>
2025-05-23 19:07:35 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,1 @@
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # Assure-toi que cette variable est bien définie dans Render
-PORT = int(os.getenv("PORT", 8000))  # Port par défaut si non défini
