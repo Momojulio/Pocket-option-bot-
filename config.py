6809df866f474ab85172421f12fa982ad2e7d2c2
Momojulio <momojulio777@gmail.com>
2025-05-23 15:04:28 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 import os
-TOKEN = os.getenv("7294172962:AAEW1JlRA2CpQ0wK3rNRe3qAjhIBxzEPZ0g")
-WEBHOOK_URL = os.getenv("https://pocket-option-bot-5b4q.onrender.com")
+TOKEN= os.getenv("7294172962:AAEW1JlRA2CpQ0wK3rNRe3qAjhIBxzEPZ0g")
+WEBHOOK_URL= os.getenv("https://pocket-option-bot-5b4q.onrender.com")
