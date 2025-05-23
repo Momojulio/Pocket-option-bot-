ca834058bc88c626a46ef1d08b9150d038bdd071
Momojulio <momojulio777@gmail.com>
2025-05-23 16:50:25 +0100

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ USE_WEBHOOK = os.getenv("USE_WEBHOOK", "True").lower() == "true"
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 TRADE_AMOUNT = float(os.getenv("TRADE_AMOUNT", 1))
 USE_REAL_ACCOUNT = os.getenv("USE_REAL_ACCOUNT", "False").lower() == "true"
+PORT=10000
