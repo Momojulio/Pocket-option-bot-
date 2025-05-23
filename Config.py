c811c0476663630a7f6237485e407591e3a09ba5
Momojulio <momojulio777@gmail.com>
2025-05-23 11:17:29 +0100

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,2 +1,2 @@
 TELEGRAM_TOKEN = "7294172962:AAEW1JlRA2CpQ0wK3rNRe3qAjhIBxzEPZ0g"
-WEBHOOK_URL = "https://pocket-option-bot-xxxxx.onrender.com"
+WEBHOOK_URL = "https://pocket-option-bot-5b4q.onrender.com"
