06e0228ca78111093ac69475dec94245e5d2a6c6
Momojulio <momojulio777@gmail.com>
2025-05-23 18:53:20 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
-USE_WEBHOOK = os.getenv("USE_WEBHOOK", "True").lower() == "true"
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
-TRADE_AMOUNT = float(os.getenv("TRADE_AMOUNT", 1))
-USE_REAL_ACCOUNT = os.getenv("USE_REAL_ACCOUNT", "False").lower() == "true"
-PORT=10000
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # Assure-toi que cette variable est bien définie dans Render
+PORT = int(os.getenv("PORT", 8000))  # Port par défaut si non défini
