28229f4cb186b3605adac5fc71b7ed1a7d2ebb76
Momojulio <momojulio777@gmail.com>
2025-05-24 13:58:16 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,11 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # Par exemple : https://pocket-option-bot-xxxxx.onrender.com
+
+
+
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # Par exemple : https://pocket-option-bot-xxxxx.onrender.com
