ec5d56650e84eeda1c0dce2053bcfa9be919fb15
Momojulio <momojulio777@gmail.com>
2025-05-24 13:57:41 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-CHAT_ID = os.environ.get("CHAT_ID")
-WEBHOOK_URL = "https://pocket-option-bot-5b4q.onrender.com"  # adapte si nécessaire
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # Par exemple : https://pocket-option-bot-xxxxx.onrender.com
